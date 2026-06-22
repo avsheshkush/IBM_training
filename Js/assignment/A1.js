4f6abf5e5809b5700d9c37c1e1e95954b6d9f355
InfernoIshu <avsheshkush@gmail.com>
2026-06-22 14:58:14 +0530

    js

---
+++ Js/assignment/A1.js
@@ -0,0 +1,35 @@
+let A=10;
+let B=10;
+let C=10;
+let N=1234;
+
+
+console.log(A + B);
+
+
+console.log(A < 10 && A > B);
+
+
+console.log(A % 10 === 0 || B % 10 === 0);
+
+
+console.log(parseInt(N / 1000));
+
+
+console.log(N % 10);
+
+
+
+console.log(B % A);
+
+
+
+console.log(A * B);
+
+
+
+let sum = A + B + C;
+let avg = (sum / 3).toFixed(2);
+
+console.log(sum);
+console.log(avg);
---
+++ Js/daily/DAY 1.js
@@ -0,0 +1,24 @@
+console.log("Ohayo");
+
+// var a=45;
+// a=70;//can be redeclared and reassign
+// console.log(a);
+
+// let b=69;
+// console.log(b);
+
+// const c =70;
+// console.log(c);
+
+
+let a =5;
+let b =10;
+let res =a+b;
+console.log(res);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+
+console.log(a===b);
+let c=66;
+console.log(c++,c,c++,c,c,c,c++,c++,c++,c);
